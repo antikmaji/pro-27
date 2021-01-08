4f6e60f9db74e9b5023f0ed0bdb1c7c1f8bb10b9
antikmaji <74954769+antikmaji@users.noreply.github.com>
2021-01-08 22:24:56 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,27 @@
+class Chain{
+    constructor(bodyA,bodyB,offsetX,offsetY){
+
+        var  options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            pointB:{x:this.offsetX,y:this.offsetY},
+            stiffness:0.1,
+            length:100,
+    }
+
+    this.chain=Constraint.create(options);
+    World.add(world,this.chain);
+
+}
+
+display(){
+
+ var pointA=this.chain.bodyA.position;
+ var pointB=this.chain.bodyB.position;
+
+ strokeWeight(1);
+
+ line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+
+}
